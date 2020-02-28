0862eb583e87e863b1adc17324a9af6777269e09
zhangml123 <s15532292192@163.com>
2020-02-29 00:33:38 +0800

    update files

--- plugin.rb
+++ plugin.rb
@@ -4,17 +4,21 @@
 # author: null
 # url: https://github.com/zhangml123/discourse_share_links
 
+class GerQrcodeController < ApplicationController
+  def index
+    qrcode_svg = RQRCode::QRCode.new("test").as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 4
+    )
 
-def get_qrcode
-  qrcode_svg = RQRCode::QRCode.new("test").as_svg(
-    offset: 0,
-    color: '000',
-    shape_rendering: 'crispEdges',
-    module_size: 4
-  )
-
-  render json: success_json.merge(
-    key: "key",
-    qr: qrcode_svg
-  )
+    render json: success_json.merge(
+      key: "key",
+      qr: qrcode_svg
+    )
+  end
+end
+Discourse::Application.routes.prepend do
+    get "/get-qrcode" => "get_qrcode#index"
 end
