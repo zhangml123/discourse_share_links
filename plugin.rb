38ea56842bec67dab954281d2e0c63e6889b658b
zhangml123 <s15532292192@163.com>
2020-02-29 01:04:01 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,22 +3,24 @@
 # version: 0.1.0
 # author: null
 # url: https://github.com/zhangml123/discourse_share_links
-require_dependency "application_controller"
-class GerQrcodeController < ::ApplicationController
-  def index
-    qrcode_svg = RQRCode::QRCode.new("test").as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 4
-    )
+after_initialize do
+  require_dependency "application_controller"
+  class GerQrcodeController < ::ApplicationController
+    def index
+      qrcode_svg = RQRCode::QRCode.new("test").as_svg(
+        offset: 0,
+        color: '000',
+        shape_rendering: 'crispEdges',
+        module_size: 4
+      )
 
-    render json: success_json.merge(
-      key: "key",
-      qr: qrcode_svg
-    )
+      render json: success_json.merge(
+        key: "key",
+        qr: qrcode_svg
+      )
+    end
   end
-end
-Discourse::Application.routes.prepend do
-    get "/get-qrcode" => "get_qrcode#index"
+  Discourse::Application.routes.prepend do
+      get "/get-qrcode" => "get_qrcode#index"
+  end
 end
